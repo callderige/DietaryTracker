60bb1ae9e12a620b99d61a9c75fb4062266e5a7e
Cliff Allderige <ca.allderige@gmail.com>
2017-05-10 11:03:28 -0400

    Removed unused imports

--- app/src/main/java/com/example/cliff/dietarytracker/EditActivity.java
+++ app/src/main/java/com/example/cliff/dietarytracker/EditActivity.java
@@ -1,6 +1,5 @@
 package com.example.cliff.dietarytracker;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,7 +7,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -136,7 +134,6 @@ public class EditActivity extends AppCompatActivity {
                         ((EditText) view2).setText(row.split("\\|")[i]);
                         ((EditText) view2).setHint(columnHints[0][i-1]+" : "+row.split("\\|")[i]);
                         break;
-
 
                     case "cardio":
                         ((EditText) view2).setText(row.split("\\|")[i]);
--- app/src/main/java/com/example/cliff/dietarytracker/EntryActivity.java
+++ app/src/main/java/com/example/cliff/dietarytracker/EntryActivity.java
@@ -1,18 +1,12 @@
 package com.example.cliff.dietarytracker;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
 @SuppressWarnings("deprecation")
 public class EntryActivity extends AppCompatActivity {
